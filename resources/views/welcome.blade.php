f5fd400d95ca72297aabc70beb3702f415a1a29c
James <jamesonyemi@gmail.com>
2022-08-01 12:02:27 +0000

    added favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" type="image/svg+xml" sizes="16x16" href="{{ asset('favicon/favicon.svg') }}favicon.svg">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon.png') }}">
     <style>
         [x-cloak] {
-          display: none;
+            display: none;
         }
     </style>
     @livewireStyles
@@ -78,7 +80,13 @@
                             <div class="rounded-md shadow">
                                 <a href="{{ route('products.index') }}"
                                     class="w-full flex items-center justify-center px-8 py-1 border border-transparent text-base font-medium rounded-md text-white bg-gray-700 hover:bg-gray-800 md:py-1 md:text-lg md:px-10">
-                                    <svg aria-hidden="true" class="animate-bounce text-orange-300 w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                                    <svg aria-hidden="true" class="animate-bounce text-orange-300 w-6 h-6 mr-2"
+                                        fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
+                                        </path>
+                                    </svg>
                                     Go Shopping </a>
                             </div>
                         </div>
