825aabdfcb4c4b2bf5e6172270a1b313561103ce
James <jamesonyemi@gmail.com>
2022-08-02 17:34:36 +0000

    session flash message

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Products;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
